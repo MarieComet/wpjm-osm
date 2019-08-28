70e0b6e81ccffc43d112a5ae10d3490833a3c171
Marie Comet <Marie Comet>
2019-08-28 16:05:46 +0200

    first commit

---
+++ assets/map.js
@@ -0,0 +1,238 @@
+(function($) {
+
+	/*
+	*  new_map
+	*
+	*  This function will render a Leaflet Map onto the selected jQuery element
+	*
+	*  @type	function
+	*  @date	8/11/2013
+	*  @since	4.3.0
+	*
+	*  @param	$el (jQuery element)
+	*  @return	n/a
+	*/
+
+	function new_map( $el ) {
+		
+		// create map	        	
+		var map = L.map( $el[0] ).setView([0, 0], 16);
+
+		L.tileLayer('https://a.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			scrollWheelZoom: false,
+			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+				'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery Â© <a href="https://www.mapbox.com/">Mapbox</a>',
+			id: 'mapbox.streets'
+		}).addTo(map);
+
+		var layerGroup = L.layerGroup().addTo(map);
+		
+		// when WPJM job listing results update
+		$( 'div.job_listings' ).on( 'updated_results', function() {
+
+			layerGroup.clearLayers();
+		
+			// var
+			var $markers = $( 'ul.job_listings' ).find( 'li.job_listing' );
+
+			
+			// add a markers reference
+			map.markers = [];
+			
+			
+			// add markers
+			$markers.each( function() {
+
+				// add marker only if latitude and longitude exists
+				if ( $(this).attr('data-latitude') && $(this).attr('data-longitude') ) {
+		    		add_marker( $(this), map, layerGroup );
+		    	}
+				
+			});
+
+			// center map
+			center_map( map );
+
+		});
+		
+		// return
+		return map;
+		
+	}
+
+	function new_single_map( $el ) {
+		
+		// create map	        	
+		var map = L.map( $el[0] ).setView([0, 0], 16);
+
+		L.tileLayer('https://a.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			scrollWheelZoom: false,
+			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+				'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery Â© <a href="https://www.mapbox.com/">Mapbox</a>',
+			id: 'mapbox.streets'
+		}).addTo(map);
+
+		var layerGroup = L.layerGroup().addTo(map);
+		
+
+
+		layerGroup.clearLayers();
+		
+		// var
+		var $markers = $( $el ).find( '.job-listing-marker' );
+
+			
+		// add a markers reference
+		map.markers = [];
+			
+			
+		// add markers
+		$markers.each( function() {
+
+			// add marker only if latitude and longitude exists
+			if ( $(this).attr('data-latitude') && $(this).attr('data-longitude') ) {
+		    	add_marker( $(this), map, layerGroup );
+		    }
+				
+		});
+
+		// center map
+		center_map( map );
+		
+		// return
+		return map;
+		
+	}
+
+	/*
+	*  add_marker
+	*
+	*  This function will add a marker to the selected Leaflet Map
+	*
+	*  @type	function
+	*  @date	8/11/2013
+	*  @since	4.3.0
+	*
+	*  @param	$marker (jQuery element)
+	*  @param	map (Leaflet Map object)
+	*  @return	n/a
+	*/
+
+	function add_marker( $marker, map, layerGroup ) {
+
+		var title = $marker.find( '.position h3' ).text();
+
+		// job listings list
+		if ( title.length ) {
+			var location = $marker.find( '.location' ).text();
+			var permalink = $marker.find( 'a' ).attr( 'href' );
+			var contentPopup = '<a href="' + permalink + '" class="wpjm-osm-title">' + title + '</a><p class="wpjm-osm-location">' + location + '<p>';
+		}
+		// var
+		var marker = L.marker(
+			[$marker.attr('data-latitude'), $marker.attr('data-longitude')],
+			{
+				title: title,
+				alt: title,
+			}
+			)
+			.addTo(layerGroup);
+
+		if ( contentPopup ) {
+			marker.bindPopup( contentPopup );
+		}
+
+		// add to array
+		map.markers.push( marker );
+
+	}
+
+	/*
+	*  center_map
+	*
+	*  This function will center the map, showing all markers attached to this map
+	*
+	*  @type	function
+	*  @date	8/11/2013
+	*  @since	4.3.0
+	*
+	*  @param	map (Leaflet Map object)
+	*  @return	n/a
+	*/
+
+	function center_map( map ) {
+
+		if ( map.markers.length !== 0 ) {
+
+			// vars
+			var bounds = L.latLngBounds();
+
+			// loop through all markers and create bounds
+			$.each( map.markers, function( i, marker ){
+
+				var latlng = [ marker._latlng.lat, marker._latlng.lng ];
+
+				bounds.extend( latlng );
+
+			});
+
+			if ( bounds ) {
+
+				// only 1 marker?
+				if( map.markers.length == 1 )
+				{
+					// set center of map
+				    //map.setCenter( bounds.getCenter() );
+				    map.fitBounds( bounds );
+				    map.setZoom( 16 );
+				}
+				else
+				{
+					// fit to bounds
+					map.fitBounds( bounds );
+				}
+
+			}
+		}
+	}
+
+
+	/*
+	*  document ready
+	*
+	*  This function will render each map when the document is ready (page has loaded)
+	*
+	*  @type	function
+	*  @date	8/11/2013
+	*  @since	5.0.0
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+	// global var*/
+	var map = null;
+
+	$(document).ready(function(){
+
+		if ( $( '#jobsMap').length ) {
+
+			map = new_map( $( '#jobsMap' ) );
+
+			map.scrollWheelZoom.disable();
+			
+		}
+
+		if ( $( '#jobSingleMap' ).length ) {
+
+			map = new_single_map( $( '#jobSingleMap' ) );
+
+			map.scrollWheelZoom.disable();
+		}
+
+	});
+
+})(jQuery);
---
+++ wpjm-osm.php
@@ -0,0 +1,187 @@
+<?php
+/*
+ * Plugin Name: WP Job Manager - Open Street Map
+ * Description: Replace Google Map by Open Street Map and Leaflet in WP Job Manager
+ * Author:      Marie Comet
+ * Author URI:  https://mariecomet.fr
+ * Version:     1.0.0
+ * Text Domain: wpjm-osm
+ * Domain Path: /languages/
+ * License:     GPL v2 or later
+ */
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+
+add_action( 'wp_enqueue_scripts', 'wpjm_osm_script' );
+function wpjm_osm_script() {
+
+	wp_register_style( 'leaflet-css', plugin_dir_url( __FILE__ ) . 'assets/leaflet/leaflet.css' );
+	wp_register_script( 'leaflet-js', plugin_dir_url( __FILE__ ) . 'assets/leaflet/leaflet.js', array( 'jquery' ) );
+	wp_register_script( 'leaflet-map', plugin_dir_url( __FILE__ ) . 'assets/map.js', array( 'jquery', 'leaflet-js' ) );
+	wp_register_style( 'wpjm-osm-style', plugin_dir_url( __FILE__ ) . 'assets/wpjm-osm-style.css' );
+
+	if ( is_singular( 'job_listing' ) ) {
+		wp_enqueue_style( 'leaflet-css' );
+		wp_enqueue_script( 'leaflet-js' );
+		wp_enqueue_script( 'leaflet-map' );
+		wp_enqueue_style( 'wpjm-osm-style' );
+	}
+
+}
+
+add_action( 'plugins_loaded', 'wpjm_osm_plugins_loaded' );
+function wpjm_osm_plugins_loaded() {
+
+
+	add_action( 'after_setup_theme', 'wpjm_osm_after_setup_theme', 2 );
+
+	// remove WPJM geocode functions, replace them with OSM geocoding
+	if( class_exists( 'WP_Job_Manager_Geocode' ) ) {
+
+		remove_action( 'job_manager_update_job_data', array( WP_Job_Manager_Geocode::instance(), 'update_location_data' ), 20 );
+		remove_action( 'job_manager_job_location_edited', array( WP_Job_Manager_Geocode::instance(), 'change_location_data' ), 20 );
+
+		add_action( 'job_manager_update_job_data', 'wpjm_osm_update_location_data', 20, 2 );
+		add_action( 'save_post', 'wpjm_osm_update_location_data', 10, 3 );
+
+	}
+}
+
+function wpjm_osm_after_setup_theme() {
+
+	add_filter( 'job_manager_output_jobs_defaults', 'wpjm_osm_job_manager_output_jobs_defaults', 10, 1 );
+	add_action( 'job_manager_job_filters_after', 'wpjm_osm_job_manager_job_filters_after' );
+	add_filter( 'the_job_location_map_link', 'wpjm_osm_the_job_location_map_link', 10, 2 );
+	add_action( 'single_job_listing_start', 'wpjm_osm_single_job_listing_start', 40 );
+
+}
+
+/**
+ * Update job listing location data
+ *
+ * @since 1.0.0
+ *
+ * @param int   $job_id The post ID.
+ * @param int   $post (WP_Post) Post object.
+ * @param int   $job_id The post ID.
+ * @return
+ */
+function wpjm_osm_update_location_data( $job_id, $post, $update ) {
+
+	$post_type = get_post_type( $job_id );
+
+	if ( "job_listing" != $post_type ) return;
+
+	// If this is just a revision, return
+	if ( wp_is_post_revision( $job_id ) ) {
+		return;
+    }
+
+	if ( isset( $_POST['_job_location'] ) && !empty( $_POST['_job_location'] ) ) {
+
+		$complete_adress = wp_strip_all_tags( $_POST['_job_location'] );
+		$existing_address = get_post_meta( $job_id, 'job_location', true );
+
+		// Construct adress meta check if address is not empty and if different of existing address. If no do not call OSM api 
+		if ( $complete_adress != $existing_address ) {
+
+			$map_url = 'https://nominatim.openstreetmap.org/search/' . $complete_adress . '?format=json&addressdetails=1&limit=1';
+
+			$request = wp_remote_get( $map_url );
+
+			if ( is_array( $request ) && ! is_wp_error( $request ) ) {
+				$json = wp_remote_retrieve_body( $request );
+
+				if( empty( $json ) ) {
+					update_post_meta( $job_id, 'geolocated', 0 );
+					return false;
+				}
+
+				$json = json_decode( $json );
+				$lat = $json[0]->lat;
+				$long = $json[0]->lon;
+				$display_name = $json[0]->display_name;
+
+				if ( $lat  && '' != $lat ) {
+					update_post_meta( $job_id, 'geolocation_lat', $lat );
+				}
+
+				if ( $long  && '' != $long ) {
+					update_post_meta( $job_id, 'geolocation_long', $long );
+				}
+
+				if ( $complete_adress  && '' != $complete_adress ) {
+					update_post_meta( $job_id, 'job_location', $complete_adress );
+					update_post_meta( $job_id, 'geolocated', 1 );
+				}
+
+				if ( $display_name  && '' != $display_name ) {
+					update_post_meta( $job_id, 'geolocation_formatted_address', $display_name );
+				}
+			}
+		}
+	}
+}
+
+/**
+ * Set default value for "show_map" attribute in [jobs] shortcode
+ *
+ * @since 1.0.0
+ * @param array $atts [jobs] shortcode attributes
+ * @return array $atts
+ */
+function wpjm_osm_job_manager_output_jobs_defaults( $atts ) {
+
+	$atts['show_map'] = 0;
+	return $atts;
+}
+
+/**
+ * Display Map on job listings list and enqueue scripts & styles
+ *
+ * @since 1.0.0
+ * @param array $atts [jobs] shortcode attributes
+ */
+function wpjm_osm_job_manager_job_filters_after( $atts ) {
+	
+	if ( isset( $atts[ 'show_map' ] ) && $atts[ 'show_map' ] ) {
+		wp_enqueue_style( 'leaflet-css' );
+		wp_enqueue_script( 'leaflet-js' );
+		wp_enqueue_script( 'leaflet-map' );
+		wp_enqueue_style( 'wpjm-osm-style' );
+		echo '<div id="jobsMap" class="wpjm-osm-map"></div>';
+	}
+}
+
+/**
+ * Filter the location for the job listing.
+ *
+ * @since 1.0.0
+ * @param  string $location job location post meta
+ * @param int|WP_Post $post
+ * @return string $map_link 
+ */
+function wpjm_osm_the_job_location_map_link( $location, $post ) {
+
+	$map_link = '<a class="osm_map_link" href="' . esc_url( 'https://www.openstreetmap.org/search?query=' . rawurlencode( wp_strip_all_tags( $location ) ) ) . '">' . esc_html( wp_strip_all_tags( $location ) ) . '</a>';
+
+	return $map_link;
+}
+
+/**
+ * Displays the map on single job listing
+ *
+ * @since 1.0.0
+ */
+function wpjm_osm_single_job_listing_start() {
+	if ( !get_the_job_location() ) {
+		return;
+	}
+	global $post;
+	?>
+	<div id="jobSingleMap" class="wpjm-osm-map">
+		<li class="job-listing-marker" data-longitude="<?php echo esc_attr( $post->geolocation_long ); ?>" data-latitude="<?php echo esc_attr( $post->geolocation_lat ); ?>" data-title="<?php echo esc_attr( $post->post_title ); ?>"></li>
+	</div>
+	<?php
+
+}
